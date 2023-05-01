ab14685ff35b5af1021ba442e3d6be1c6d532ceb
Michał Mazurek <mmazurekgda@gmail.com>
2023-05-01 21:03:45 +0200

    Add the basic functionality of an Inferable::Entity

--- lib/inferable.rb
+++ lib/inferable.rb
@@ -1,8 +1,42 @@
 # frozen_string_literal: true
 
+require "active_support"
+require "active_record"
 require_relative "inferable/version"
 
-module Inferable
-  class Error < StandardError; end
-  # Your code goes here...
+# acts_as_inferable.rb
+module ActiveRecord
+  class Base
+    def self.acts_as_inferable(*args)
+      send(:include, ::Inferable::Entity)
+      define_method(:features) { args }
+    end
+  end
 end
+
+module Inferable
+  class PendingRecord < ActiveRecord::Base
+    belongs_to :inferable, polymorphic: true, optional: true
+    self.table_name = "inferable_pending_records"
+  end
+
+  module Entity
+    extend ActiveSupport::Concern
+    included do
+      has_many :pending_records, as: :inferable, class_name: "Inferable::PendingRecord", dependent: :nullify
+      after_update :create_pending_record, if: :any_feature_changed?
+      after_create :create_pending_record
+    end
+
+    def create_pending_record
+      PendingRecord.new(inferable: self).save!
+    end
+
+    def any_feature_changed?
+      features.any? { |feature| saved_change_to_attribute? feature }
+    end
+
+  end
+end
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "inferable"
+require "sqlite3"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -12,4 +13,26 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+
+ActiveRecord::Schema.define(version: 1) do
+  create_table :inferable_pending_records do |t|
+    t.references :inferable, polymorphic: true, index: { name: :inferable_pending_records_index }
+    t.datetime :created_at, null: false
+  end
+
+  create_table :inferable_entities do |t|
+    t.float :inferable_feature, default: 0
+    t.float :non_inferable_feature, default: 0
+  end
+end
+
+class PlainEntity < ActiveRecord::Base
+  self.table_name = "inferable_entities"
+end
+
+class InferableEntity < PlainEntity
+  acts_as_inferable :inferable_feature
 end
